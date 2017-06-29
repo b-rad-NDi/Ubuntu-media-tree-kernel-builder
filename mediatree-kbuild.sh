d321b4c8f5a6fd4fe3a35f0385d3bdefe3d9e79b
Brad Love <brad@nextdimension.cc>
2017-06-28 19:38:43 -0500

    switch to /etc/os-release for distro codename, genericize var

--- mediatree-kbuild.sh
+++ mediatree-kbuild.sh
@@ -48,13 +48,13 @@ if [ -z "${DISTRO_NAME}" ] ; then
 	DISTRO_NAME=${ID}
 fi
 
-if [ -z "${UBUNTU_VERSION}" ] ; then
-	if [ ! -f /etc/lsb-release ] ; then
-		echo "No /etc/lsb-release, cannot determine running Ubuntu..."
+if [ -z "${DISTRO_CODENAME}" ] ; then
+	if [ ! -f /etc/os-release ] ; then
+		echo "No /etc/os-release, cannot determine running OS..."
 		exit 250
 	fi
-	eval `cat /etc/lsb-release`
-	UBUNTU_VERSION=${DISTRIB_CODENAME}
+	eval `cat /etc/os-release`
+	DISTRO_CODENAME=${VERSION_CODENAME}
 fi
 
 #[ -z "${UBUNTU_REVISION}" ] && UBUNTU_REVISION=50aaaec159365f8f8788e054048545e7ec9734f1
@@ -80,11 +80,11 @@ fi
 
 function get_ubuntu_kver()
 {
-	export KVER=`grep VERSION ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}/Makefile | head -n 1 | cut -d ' ' -f 3`
-	export KMAJ=`grep PATCHLEVEL ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}/Makefile | head -n 1 | cut -d ' ' -f 3`
+	export KVER=`grep VERSION ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}/Makefile | head -n 1 | cut -d ' ' -f 3`
+	export KMAJ=`grep PATCHLEVEL ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}/Makefile | head -n 1 | cut -d ' ' -f 3`
 	export KMIN=0
-	export K_ABI_A=`head -n1 ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}/debian.master/changelog | cut -d'-' -f2 | cut -d'.' -f1`
-	export K_ABI_B=`head -n1 ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}/debian.master/changelog | cut -d'.' -f4 | cut -d')' -f1`
+	export K_ABI_A=`head -n1 ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}/debian.master/changelog | cut -d'-' -f2 | cut -d'.' -f1`
+	export K_ABI_B=`head -n1 ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}/debian.master/changelog | cut -d'.' -f4 | cut -d')' -f1`
 }
 
 function write_state_env()
@@ -105,7 +105,7 @@ function get_ubuntu()
 	ret_val=0
 	cd ${TOP_DEVDIR}
 	if [ -z "${1}" ]; then
-		TARGET_DIR=${DISTRO_NAME}-${UBUNTU_VERSION}
+		TARGET_DIR=${DISTRO_NAME}-${DISTRO_CODENAME}
 	else
 		TARGET_DIR=${1}
 	fi
@@ -115,7 +115,7 @@ function get_ubuntu()
 		cd -
 		git clone ${TOP_DEVDIR}/.clean-master-repo ${TARGET_DIR}
 	elif [ ! -d "${TARGET_DIR}" ] ; then
-		git clone git://kernel.ubuntu.com/ubuntu/${DISTRO_NAME}-${UBUNTU_VERSION}.git ${TARGET_DIR}
+		git clone git://kernel.ubuntu.com/ubuntu/${DISTRO_NAME}-${DISTRO_CODENAME}.git ${TARGET_DIR}
 	fi
 
 	if [ -n "${UBUNTU_REVISION}" -a "${TARGET_DIR}" != ".clean-master-repo" ] ; then
@@ -156,7 +156,7 @@ function get_ubuntu()
 	if [ -z "${1}" ] ; then
 		get_ubuntu_kver
 		write_state_env
-		export KB_PATCH_DIR="${TOP_DEVDIR}/patches/${DISTRO_NAME}-${UBUNTU_VERSION}-${KVER}.${KMAJ}.0"
+		export KB_PATCH_DIR="${TOP_DEVDIR}/patches/${DISTRO_NAME}-${DISTRO_CODENAME}-${KVER}.${KMAJ}.0"
 	fi
 	return ${ret_val}
 }
@@ -223,7 +223,7 @@ function gen_media_tree_tarball_patched()
 	cd linux
 
 	# linuxtv media tree syslog messages and config-compat.h generation
-	perl ../v4l/scripts/make_config_compat.pl ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}/ ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}/debian.master/config/config.common.ubuntu config-compat.h
+	perl ../v4l/scripts/make_config_compat.pl ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}/ ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}/debian.master/config/config.common.ubuntu config-compat.h
 	perl ./version_patch.pl
 
 	# apply kernel version specific backport patches
@@ -242,7 +242,7 @@ function reset_repo_head_hard()
 {
 	cd ${TOP_DEVDIR}
 	if [ -z "${1}" -o ! -d "${1}" ]; then
-		TARGET_DIR=${DISTRO_NAME}-${UBUNTU_VERSION}
+		TARGET_DIR=${DISTRO_NAME}-${DISTRO_CODENAME}
 	else
 		TARGET_DIR=${1}
 	fi
@@ -265,7 +265,7 @@ function reset_ubuntu_hard()
 {
 	cd ${TOP_DEVDIR}
 	if [ -z "${1}" -o ! -d "${1}" ]; then
-		TARGET_DIR=${DISTRO_NAME}-${UBUNTU_VERSION}
+		TARGET_DIR=${DISTRO_NAME}-${DISTRO_CODENAME}
 	else
 		TARGET_DIR=${1}
 	fi
@@ -293,7 +293,7 @@ function apply_media_tree()
 {
 	cd ${TOP_DEVDIR}
 	if [ -z "${1}" ]; then
-		TARGET_DIR=${DISTRO_NAME}-${UBUNTU_VERSION}
+		TARGET_DIR=${DISTRO_NAME}-${DISTRO_CODENAME}
 	else
 		TARGET_DIR=${1}
 	fi
@@ -333,7 +333,7 @@ function configure_repo_git()
 {
 	cd ${TOP_DEVDIR}
 	if [ -z "${1}" ]; then
-		TARGET_DIR=${DISTRO_NAME}-${UBUNTU_VERSION}
+		TARGET_DIR=${DISTRO_NAME}-${DISTRO_CODENAME}
 	else
 		TARGET_DIR=${1}
 	fi
@@ -379,7 +379,7 @@ function apply_patch_git_am()
 
 function apply_patches()
 {
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	###################################################
 	########## Pure LinuxTV.org media tree ############
@@ -443,7 +443,7 @@ function apply_patches()
 
 function apply_extra_patches()
 {
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	for i in `ls ${KB_PATCH_DIR}/extra/*.patch | sort` ; do
 		echo "#############################################################"
@@ -461,7 +461,7 @@ function generate_patch_set()
 		return 3
 	fi
 
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	#	git format-patch -# HEAD
 	# Generate all patches after checkout revision
@@ -470,7 +470,7 @@ function generate_patch_set()
 
 function update_identity()
 {
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	sed -i "s/Fake User <hidden@email.co>/${U_FULLNAME} <${U_EMAIL}>/" debian.master/control.stub.in
 	sed -i "s/Fake User <hidden@email.co>/${U_FULLNAME} <${U_EMAIL}>/" debian.master/changelog
@@ -483,7 +483,7 @@ function regen_changelog()
 		return 3
 	fi
 
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	[ -z "${1}" ] && echo "error..." && return 1
 
@@ -502,7 +502,7 @@ function regen_changelog()
 
 	K_BUILD_TIME=`date -R`
 
-	echo "linux (${KVER}.${KMAJ}.${KMIN}-${K_ABI_A}${K_BUILD_VER}.${K_ABI_MOD}${KERNEL_ABI_TAG}) ${UBUNTU_VERSION}; urgency=low" > /tmp/tmpkrn_changelog.mod
+	echo "linux (${KVER}.${KMAJ}.${KMIN}-${K_ABI_A}${K_BUILD_VER}.${K_ABI_MOD}${KERNEL_ABI_TAG}) ${DISTRO_CODENAME}; urgency=low" > /tmp/tmpkrn_changelog.mod
 	echo "" >>  /tmp/tmpkrn_changelog.mod
 	echo "  * Ubuntu kernel git clone">>  /tmp/tmpkrn_changelog.mod
 	echo "    - ${KVER}.${KMAJ}.${KMIN}-${K_ABI_A}.${K_ABI_B} rev ${UBUNTU_REVISION}">>  /tmp/tmpkrn_changelog.mod
@@ -548,14 +548,14 @@ function regen_changelog()
 
 function clean_kernel()
 {
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 	fakeroot debian/rules clean
 	return 0
 }
 
 function generate_new_kernel_version()
 {
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	regen_changelog "`date +%Y%m%d%H%M`"
 	update_identity
@@ -569,12 +569,12 @@ function generate_virtual_package()
 {
 	cd ${TOP_DEVDIR}
 
-	if [ ! -f "${DISTRO_NAME}-${UBUNTU_VERSION}/debian/changelog" ] ; then
+	if [ ! -f "${DISTRO_NAME}-${DISTRO_CODENAME}/debian/changelog" ] ; then
 		echo "Error, you must 'clean'/regenerate build data first"
 		return 1
 	fi
 
-	LAST_KBUILD_VER=`head -n 1 ${DISTRO_NAME}-${UBUNTU_VERSION}/debian/changelog | egrep -o '201[7-9][[:digit:]]{8}'`
+	LAST_KBUILD_VER=`head -n 1 ${DISTRO_NAME}-${DISTRO_CODENAME}/debian/changelog | egrep -o '201[7-9][[:digit:]]{8}'`
 
 	VPACKAGE_VER=`head -n 1 changelog`
 	cd .vpackage_tmp
@@ -584,7 +584,7 @@ function generate_virtual_package()
 
 	if [ "${1}" == "image" -o "${1}" == "headers" ] ; then
 		cp ../linux-${1}-mediatree.control ./ns_control
-		echo "linux-${1}-mediatree (${VPACKAGE_VER}+${UBUNTU_VERSION}) ${UBUNTU_VERSION}; urgency=low" > changelog
+		echo "linux-${1}-mediatree (${VPACKAGE_VER}+${DISTRO_CODENAME}) ${DISTRO_CODENAME}; urgency=low" > changelog
 		git log --pretty=format:"  * %h %s" -n 13 >> changelog
 		echo "" >> changelog
 		echo "" >> changelog
@@ -610,13 +610,13 @@ function generate_virtual_package()
 	echo "#########################################"
 	echo "#########################################"
 	equivs-build --full ns_control
-	if [ ! -f "linux-${1}-mediatree_${VPACKAGE_VER}+${UBUNTU_VERSION}.dsc" ] ; then
-		echo "Missing linux-${1}-mediatree_${VPACKAGE_VER}+${UBUNTU_VERSION}.dsc"
+	if [ ! -f "linux-${1}-mediatree_${VPACKAGE_VER}+${DISTRO_CODENAME}.dsc" ] ; then
+		echo "Missing linux-${1}-mediatree_${VPACKAGE_VER}+${DISTRO_CODENAME}.dsc"
 		return 1
 	fi
-	dpkg-source -x linux-${1}-mediatree_${VPACKAGE_VER}+${UBUNTU_VERSION}.dsc
+	dpkg-source -x linux-${1}-mediatree_${VPACKAGE_VER}+${DISTRO_CODENAME}.dsc
 	[ $? -ne 0 ] && return 1
-	cd linux-${1}-mediatree-${VPACKAGE_VER}+${UBUNTU_VERSION}
+	cd linux-${1}-mediatree-${VPACKAGE_VER}+${DISTRO_CODENAME}
 	debuild -us -uc -S
 	cd ..
 	cp linux-${1}-mediatree*.tar.gz ..
@@ -628,7 +628,7 @@ function generate_virtual_package()
 
 function build_kernel_bin()
 {
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 
 	if [ "$1" == "debug" ] ; then
 		time fakeroot debian/rules binary-headers binary-generic binary-perarch skipdbg=false
@@ -650,7 +650,7 @@ function generate_ppa_data()
 	read x
 	[ "${x}" != "YES" ] && return 1
 
-	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${UBUNTU_VERSION}
+	cd ${TOP_DEVDIR}/${DISTRO_NAME}-${DISTRO_CODENAME}
 	debuild -us -uc -S
 
 	generate_virtual_package image
@@ -677,7 +677,7 @@ function init_mediatree_builder()
 	echo ""
 
 	cd ${TOP_DEVDIR}
-	if [ ! -d "${DISTRO_NAME}-${UBUNTU_VERSION}" ] ; then
+	if [ ! -d "${DISTRO_NAME}-${DISTRO_CODENAME}" ] ; then
 		echo "Initializing Ubuntu work repo"
 		get_ubuntu
 		configure_repo_git
@@ -752,7 +752,7 @@ while getopts ":imMrxCcgbB:spV:" o; do
 		# resets Ubuntu git back to original commit package is based on
 		# suitable for re applying updated patchset
 		# WARNING: irreversibly wipes out all files and resets to original state!!!
-		echo "!!! Requesting hard reset of ${DISTRO_NAME}-${UBUNTU_VERSION}"
+		echo "!!! Requesting hard reset of ${DISTRO_NAME}-${DISTRO_CODENAME}"
 		echo "!!!  This will irrerversibly wipe out the entire directory and restore it to original state"
 		echo "!!!  Any changes you have made will be lost."
 		reset_ubuntu_hard
